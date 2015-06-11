613202191576b8cb67147f97d81baea976dd97d1
Brandon Amos <bdamos@vt.edu>
2015-06-11 10:10:26 -0400

    CC me on reports.

--- email-report.sh
+++ email-report.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 cd $(dirname $0)
-./report.py | mutt cs-conference-tracker@googlegroups.com \
+./report.py | mutt cs-conference-tracker@googlegroups.com bamos@cs.cmu.edu \
   -s "Conference Status for $(date +%Y-%m-%d)"
